4b2a771dddf9454db015994a5e91e04da14c5194
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-06-24 00:03:34 +0200

    add app to installer

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -6,4 +6,26 @@ use phpOMS\Account\PermissionType;
 use phpOMS\Router\RouteVerb;
 
 return [
+	'^.*/contract/list.*$' => [
+        [
+            'dest'       => '\Modules\ContractManagement\Controller\BackendController:viewContractList',
+            'verb'       => RouteVerb::GET,
+            'permission' => [
+                'module' => BackendController::MODULE_NAME,
+                'type'   => PermissionType::READ,
+                'state'  => PermissionState::CONTRACT,
+            ],
+        ],
+    ],
+    '^.*/contract/single.*$' => [
+        [
+            'dest'       => '\Modules\ContractManagement\Controller\BackendController:viewContract',
+            'verb'       => RouteVerb::GET,
+            'permission' => [
+                'module' => BackendController::MODULE_NAME,
+                'type'   => PermissionType::READ,
+                'state'  => PermissionState::CONTRACT,
+            ],
+        ],
+    ],
 ];
