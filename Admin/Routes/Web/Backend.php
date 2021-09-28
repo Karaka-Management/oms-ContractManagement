4143eff4ec2ecb6b02f6db2836b07fe92d66d777
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-09-28 18:28:42 +0200

    rename module constants and fix some unit test bugs

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -23,7 +23,7 @@ return [
             'dest'       => '\Modules\ContractManagement\Controller\BackendController:viewContractList',
             'verb'       => RouteVerb::GET,
             'permission' => [
-                'module' => BackendController::MODULE_NAME,
+                'module' => BackendController::NAME,
                 'type'   => PermissionType::READ,
                 'state'  => PermissionState::CONTRACT,
             ],
@@ -34,7 +34,7 @@ return [
             'dest'       => '\Modules\ContractManagement\Controller\BackendController:viewContract',
             'verb'       => RouteVerb::GET,
             'permission' => [
-                'module' => BackendController::MODULE_NAME,
+                'module' => BackendController::NAME,
                 'type'   => PermissionType::READ,
                 'state'  => PermissionState::CONTRACT,
             ],
