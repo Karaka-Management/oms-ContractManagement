53f95ed9628713d2cafb734d6491505674c410ee
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-09 23:02:52 +0100

    reduce serialization of null models

--- Models/NullContract.php
+++ Models/NullContract.php
@@ -36,4 +36,12 @@ final class NullContract extends Contract
         $this->id = $id;
         parent::__construct();
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
--- Models/NullContractType.php
+++ Models/NullContractType.php
@@ -36,4 +36,12 @@ final class NullContractType extends ContractType
         $this->id = $id;
         parent::__construct();
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
